6601bd41b0abe45acee718266a879160ca885a11
DongJoonLeeDJ <ldjgood12@gmail.com>
2023-05-24 10:52:36 +0900

    2Day

--- src/Ch01/C02SystemOut.java
+++ src/Ch01/C02SystemOut.java
@@ -25,15 +25,23 @@ public class C02SystemOut {
 		
 		
 		//System.out.printf()
-		
-		System.out.printf("%d %d %d \n"	,10,20,30);
-		System.out.printf("%f %f %f \n"	,10.1,20.1,30.1);
-		System.out.printf("%c %c %c\n"	, 'A','B','C');
-		System.out.printf("%s %s %s\n"	, "This is"," String ", "Test");
+		//format : 형식,서식
+		//%d : 10진수 정수 서식문자
+		//%f : 10진수 실수 서식문자
+		//%c : 한문자 서식문자
+		//%s : 문자열 서식문자
+		System.out.printf("%d %d %d \n"	, 10,20,30);
+		System.out.printf("%f %f %f \n"	, 10.1,20.1,30.1);
+		System.out.printf("%c %c %c \n"	, 'A','B','C');
+		System.out.printf("%s %s %s \n"	, "This is"," String ", "Test");
 		System.out.printf("%d.%s : %d\n", 1, "국어",100);
 		
 		
 		//System.out.println()
+		System.out.println("HELLO WORLD");
+		System.out.println("HELLO WORLD");
+		System.out.println("HELLO WORLD");
+		
 		
 		
 		
