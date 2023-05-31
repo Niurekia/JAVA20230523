5f0f2b2193ca5d79cad9d0e672ae6954d286e4dd
jungWooGyun <jwg8910@naver.com>
2023-05-31 09:45:11 +0900

    4Day

---
+++ src/Ch07/C00While.java
@@ -0,0 +1,107 @@
+package Ch07;
+
+import java.util.Scanner;
+
+public class C00While {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+		//01 반복문 기본
+		
+//		int i=1;		// 1 탈출용 변수
+//		while(i<=10) 	// 2 탈출 조건식
+//		{
+//			System.out.println("HELLO WORLD");
+//			i++;		// 3 탈출을 위한 연산
+//		}
+//		System.out.println("i : " + i);
+		
+
+		//02 1 - 10까지의 합
+		
+//		int i=1;		// 1 탈출용 변수
+//		int sum=0;
+//		while(i<=10) 	// 2 탈출 조건식
+//		{
+//			System.out.println("i : " + i);
+//			sum = sum + i;
+//			i++;		// 3 탈출을 위한 연산
+//		}
+//		System.out.println("SUM : " + sum);
+		
+		//04 1 - N 까지의 합
+//		Scanner sc = new Scanner(System.in);
+//		int n = sc.nextInt();
+//		int i=1;		// 1 탈출용 변수
+//		int sum=0;
+//		while(i<=n) 	// 2 탈출 조건식
+//		{
+//			System.out.println("i : " + i);
+//			sum = sum + i;
+//			i++;		// 3 탈출을 위한 연산
+//		}
+//		System.out.println("SUM : " + sum);
+//		sc.close();
+		
+		//문제
+		// N - M 까지의 합을 구합니다.(N<M)
+		
+//		Scanner sc = new Scanner(System.in);
+//		int n = sc.nextInt();
+//		int m = sc.nextInt();
+//		
+//		if(n>m) {
+//			int tmp=n;
+//			n=m;
+//			m=tmp;
+//		}
+//		
+//		int i=n;		// 1 탈출용 변수
+//		int sum=0;
+//		while(i<=m) 	// 2 탈출 조건식
+//		{
+//			System.out.println("i : " + i);
+//			sum = sum + i;
+//			i++;		// 3 탈출을 위한 연산
+//		}
+//		System.out.println("SUM : " + sum);
+//		sc.close();
+		
+		
+		
+		//01 1 - N 까지 수중에 짝수/홀수의 합을 각각 출력
+//		Scanner sc = new Scanner(System.in);
+//		int n = sc.nextInt();
+//		int i=1;		// 1 탈출용 변수
+//		int sum=0;
+//		int odd=0;		//홀수 누적합용 변수
+//		int even=0;		//짝수 누적합용 변수
+//		while(i<=n) 	// 2 탈출 조건식
+//		{
+//			System.out.println("i : " + i);
+//			sum +=i;
+//			if(i%2==0) {
+//				even+=i;
+//			}else {
+//				odd+=i;
+//			}
+//			i++;		// 3 탈출을 위한 연산
+//		}
+//		System.out.println("SUM : " + sum);
+//		System.out.println("짝수합 : " + even);
+//		System.out.println("홀수합 : " + odd);
+//		sc.close();
+		
+		//1 - N 까지 수중에 3의 배수만 출력하고 그합도 출력
+		//1 - N 까지 수중에 4의 배수를 출력하고 4의배수가 아닌 나머지의 합을 구하세요
+		
+		
+		
+		
+
+	}
+
+}
