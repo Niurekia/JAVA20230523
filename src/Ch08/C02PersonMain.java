e32ecb27bb13a15a7b8cc85190f39cc42cf5e293
jungWooGyun <jwg8910@naver.com>
2023-06-01 13:09:50 +0900

    05DAY

--- src/Ch08/C02PersonMain.java
+++ src/Ch08/C02PersonMain.java
@@ -1,7 +1,7 @@
 package Ch08;
 
 
-public class C01Person {
+class C02Person {
 	//속성
 	String name;
 	int age;
@@ -12,9 +12,14 @@ public class C01Person {
 
 public class C02PersonMain {
 
-	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		C02Person hong = new C02Person();
+		hong.name="홍길동";
+		hong.age = 55;
+		hong.height=177.5f;
+		hong.weight=70.4;
+		System.out.printf("%s %d %f %f\n", hong.name,hong.age,hong.height,hong.weight);
+		
 
 	}
 
