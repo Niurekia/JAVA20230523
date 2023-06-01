c1fc062bc8f0d12dcbc2fe53d5d27a116ad3e461
jungWooGyun <jwg8910@naver.com>
2023-06-01 13:05:35 +0900

    05DAY

---
+++ src/Ch08/C02PersonMain.java
@@ -0,0 +1,21 @@
+package Ch08;
+
+
+public class C01Person {
+	//속성
+	String name;
+	int age;
+	float height;	//신장
+	double weight;	//몸무게
+	//기능
+}
+
+public class C02PersonMain {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
